2518bbbf8128979293269715aa46cefba772ef57
guilhermepeppi <guilhermepeppi@gmail.com>
2024-04-02 09:18:04 -0300

    Upload

--- Ex19.py
+++ Ex19.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça dois números e imprima o maior deles.
+
+numero1 = float(input('Digite o primeiro numero: '))
+numero2 = float(input('Digite o segundo numero: '))
+
+if numero1 > numero2:
+    print(f'{numero1} é maior que {numero2}')
+else:
+    print(f'{numero2} é maior que {numero1}')
