131ae4d11dc05f46306a4f20fabebd6779578e00
guilhermepeppi <guilhermepeppi@gmail.com>
2024-04-11 12:24:06 -0300

    Upload

--- Ex103.py
+++ Ex103.py
@@ -5,14 +5,26 @@
 idades = []
 alturas = []
 media = 0
+total_alunos_maior_idade = 0
 
 for i in range(5):
     while True:
         try:
-            idade = int(input(f'{i+1}º Aluno - idade'))
-            altura = float(input(f'{i+1}º Aluno - altura'))
+            idade = int(input(f'{i+1}º Aluno - idade: '))
+            altura = float(input(f'{i+1}º Aluno - altura: '))
             idades.append(idade)
             alturas.append(altura)
             break
         except ValueError:
             print('\033[31mERRO! Digite um valor válido.\033[m')
+
+
+media = sum(alturas) / len(alturas)
+print(f'Média de altura {media:.2f}')
+
+for i in range(len(idades)):
+    if idades[i] > 13 and alturas[i] < media:
+        total_alunos_maior_idade += 1
+
+print(f'Quantidade de alunos com mais de 13 anos possuem altura inferior à média: {
+      total_alunos_maior_idade}')
